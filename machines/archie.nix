d6d0648b9d46031d08f6e2cea1151d29477308cc
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-16 19:08:40 +0000

    Update archie.nix

--- machines/archie.nix
+++ machines/archie.nix
@@ -8,6 +8,13 @@
 {
   imports = [
     ../hardware/generic.nix
+    ../programs/firefox.nix
+    ../programs/vscode.nix
+    ../programs/python.nix
+    ../programs/git.nix
+    ../programs/zsh.nix
+    ../programs/virtualisation.nix
+    ../programs/node.nix
   ];
 
   system.autoUpgrade = {
