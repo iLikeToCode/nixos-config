9020b764a50e84d6f01a4d31a47a73c564bb7cfd
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-16 13:17:45 +0000

    Create archie.nix

---
+++ machines/archie.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}:
+{
+  imports = [
+    ../hardware/generic.nix
+  ];
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = self.outPath;
+    dates = "03:00";
+  };
+
+  programs.nh = {
+    enable = true;
+
+    clean = {
+      enable = true;
+      extraArgs = "--keep 5";
+    };
+  };
+
+  users.users.archie = {
+    description = "Archie Hurst";
+    isNormalUser = true;
+    extraGroups = [ "dialout" "networkmanager" "wheel" ];
+  };
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = "nix-command flakes";
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = lib.mkForce "uk";
+    useXkbConfig = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    chromium
+    bitwarden-desktop
+  ];
+
+  system.stateVersion = "25.11";
+}
