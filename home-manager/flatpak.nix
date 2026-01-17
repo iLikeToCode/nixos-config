a6eee74d80a362a2e06f370e22b3bd19004c7e23
iLikeToCode <archie@archiesbytes.xyz>
2026-01-17 22:13:13 +0000

    change ExecStart to script

--- home-manager/flatpak.nix
+++ home-manager/flatpak.nix
@@ -1,51 +1,53 @@
 { pkgs, ... }:
 
 {
-    systemd.user.services.flatpak-update = {
-        Service = {
-            Type = "oneshot";
-        };
-
-        serviceConfig = {
-            script = "${pkgs.flatpak}/bin/flatpak update -y";
-        };
-
-        Install = {
-            WantedBy = [ "default.target" ];
-        };
+  systemd.user.services.flatpak-install = {
+    Unit = {
+      Description = "Install user Flatpaks";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
     };
 
-    systemd.user.timers.flatpak-update = {
-        Timer = {
-            OnCalendar = "daily";
-            Persistent = true;
-        };
-        Install.WantedBy = [ "timers.target" ];
+    Service = {
+      Type = "oneshot";
     };
 
-    systemd.user.services.flatpak-install = {
-        Unit = {
-            Description = "Install user Flatpaks";
-            After = [ "network-online.target" ];
-            Wants = [ "network-online.target" ];
-        };
+    serviceConfig = {
+      script = ''
+${pkgs.flatpak}/bin/flatpak update -y || true
 
-        Service = {
-            Type = "oneshot";
-        };
-
-        serviceConfig = {
-            script = ''
-            ${pkgs.flatpak}/bin/flatpak update -y || true
-
-            ${pkgs.flatpak}/bin/flatpak install -y \
-                flathub \
-                org.vinegarhq.Sober
-            '';
-        };
-
-        Install = {
-            WantedBy = [ "default.target" ];
-        };
+${pkgs.flatpak}/bin/flatpak install -y \
+  flathub \
+  org.vinegarhq.Sober
+      '';
     };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.services.flatpak-update = {
+    Service = {
+      Type = "oneshot";
+    };
+
+    serviceConfig = {
+      script = ''
+${pkgs.flatpak}/bin/flatpak update -y
+      '';
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.timers.flatpak-update = {
+    Timer = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
 }
