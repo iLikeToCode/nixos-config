4f1875e432159638e7a87d390a22b55c1e701b54
iLikeToCode <archie@archiesbytes.xyz>
2026-01-17 22:14:39 +0000

    rm auto flatpak

--- home-manager/archie.nix
+++ home-manager/archie.nix
@@ -3,11 +3,9 @@
 {
   imports = [
     ./git.nix
-    ./flatpak.nix
   ];
 
   home.username = "archie";
   home.homeDirectory = "/home/archie";
   home.stateVersion = "25.11";
-  home.file.".background-image".source = ./wallpaper.png;
 }
