6a76e0c2180ed83006d6a997b09add6418ca1eda
iLikeToCode <archie@archiesbytes.xyz>
2026-01-21 21:24:26 +0000

    allow more ports

--- machines/AH-W.nix
+++ machines/AH-W.nix
@@ -5,7 +5,7 @@
   ];
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#ah-w";
-  networking.firewall.allowedTCPPorts = [ 8080 8081 ];
+  networking.firewall.allowedTCPPorts = [ 443 3000 8080 8081 ];
 
   networking.hostName = "AH-W";
 }
