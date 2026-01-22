6aaae5481a45260e3c4417df2743d1630d5c1519
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-22 15:00:49 +0000

    Update OLP-ML04.nix

--- machines/OLP-ML04.nix
+++ machines/OLP-ML04.nix
@@ -6,9 +6,9 @@
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#olp-ml04";
   environment.systemPackages =
-    builtins.filter
+    lib.mkForce builtins.filter
       (pkg: pkg != pkgs.discord)
-      config.environment.systemPackages;
+      environment.systemPackages;
 
   networking.hostName = "OLP-ML04";
 }
