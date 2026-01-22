407c7c2df4ee4c2cb15f3b046df55ec8a41edc08
iLikeToCode <archie@archiesbytes.xyz>
2026-01-22 18:23:46 +0000

    remove ports

--- machines/AH-W.nix
+++ machines/AH-W.nix
@@ -5,7 +5,6 @@
   ];
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#ah-w";
-  networking.firewall.allowedTCPPorts = [ 443 3000 8080 8081 ];
 
   networking.hostName = "AH-W";
 }
