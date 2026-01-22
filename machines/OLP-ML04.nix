e50859b057468108225be4518a77eabba7806556
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-22 14:57:02 +0000

    Filter out Discord from systemPackages
    Exclude Discord from system packages.

--- machines/OLP-ML04.nix
+++ machines/OLP-ML04.nix
@@ -5,6 +5,10 @@
   ];
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#olp-ml04";
+  environment.systemPackages =
+    builtins.filter
+      (pkg: pkg != pkgs.discord)
+      config.environment.systemPackages;
 
   networking.hostName = "OLP-ML04";
 }
