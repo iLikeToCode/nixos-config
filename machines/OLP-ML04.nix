fb79e4591c70e392372cadce7e45538b0075f47c
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-22 15:02:27 +0000

    Update systemPackages to exclude discord
    Filter out 'discord' from system packages.

--- machines/OLP-ML04.nix
+++ machines/OLP-ML04.nix
@@ -5,10 +5,11 @@
   ];
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#olp-ml04";
-  environment.systemPackages =
-    lib.mkForce builtins.filter
+  environment.systemPackages = lib.mkForce (
+    builtins.filter
       (pkg: pkg != pkgs.discord)
-      environment.systemPackages;
+      environment.systemPackages
+  ); 
 
   networking.hostName = "OLP-ML04";
 }
