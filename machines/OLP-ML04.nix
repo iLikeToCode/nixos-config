fcb04d7fca252feb6061792dce266d798c42a625
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-01-22 14:38:42 +0000

    Update OLP-ML04 configuration for hostname and flake

---
+++ machines/OLP-ML04.nix
@@ -0,0 +1,10 @@
+{ lib, ... }:
+{
+  imports = [
+    ./archie.nix
+  ];
+
+  system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#olp-ml04";
+
+  networking.hostName = "OLP-ML04";
+}
