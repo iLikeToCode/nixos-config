d2f686f347316f9df6337ee41018a974ad0a9155
iLikeToCode <archie@archiesbytes.xyz>
2026-01-31 20:35:46 +0000

    fix webots

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    webots.url = "path:./programs/packages/webots";
+    webots.url = "./programs/packages/webots";
     webots.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
