ca006e0bf7b378fe61ff7b28150686a9ec94bd65
iLikeToCode <archie@archiesbytes.xyz>
2026-03-31 14:52:23 +0100

    ovpn

--- programs/networking.nix
+++ programs/networking.nix
@@ -3,8 +3,14 @@
 {
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
+    openvpn
   ];
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+  };
   services.tailscale.enable = true;
   networking.resolvconf.enable = true;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
