3ec1b265515fb939ff3cbf9b8b02fddc943db170
iLikeToCode <archie@archiesbytes.xyz>
2026-04-05 14:59:18 +0100

    firefox

--- programs/desktop/i3-config.nix
+++ programs/desktop/i3-config.nix
@@ -74,6 +74,7 @@ in
         "${mod}+shift+d" = "exec --no-startup-id rofi -show window";
         "${mod}+shift+x" = "exec --no-startup-id i3lock --image $HOME/.lock-image";
 
+        "${mod}+f" = "exec firefox";
         "${mod}+Return" = "exec xfce4-terminal";
 
         # Screenshots (Flameshot)
