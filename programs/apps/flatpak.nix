80fa130afdbb768270bdb4077797cacd9de3ef50
iLikeToCode <archie@archiesbytes.xyz>
2026-04-15 11:10:56 +0100

    disable broken flatpak script

--- programs/apps/flatpak.nix
+++ programs/apps/flatpak.nix
@@ -13,12 +13,4 @@
             };
         };
     };
-
-    system.activationScripts.flatpakFlathub = {
-    text = ''
-        ${pkgs.flatpak}/bin/flatpak \
-        remote-add --if-not-exists \
-        flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
-    };
 }
