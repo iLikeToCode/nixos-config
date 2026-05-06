d2119aa7e9dafa2d4672de32473acde5c60be091
iLikeToCode <archie@archiesbytes.xyz>
2026-05-06 21:29:14 +0100

    fix opencv4

--- programs/apps/python-env.nix
+++ programs/apps/python-env.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 
-pkgs.python313.withPackages (python-pkgs: with python-pkgs // { opencv4 = pkgs.callPackage ../packages/opencv4.nix; }; [
+pkgs.python313.withPackages (python-pkgs: with python-pkgs; [
     torch
     torchvision
     torchaudio
-    (opencv4.override {
+    (python-pkgs.opencv4.override {
         enableGtk3 = true;
     })
     facenet-pytorch
