f9da2c742e680a2a29f222ef44724bd63981baa3
iLikeToCode <archie@archiesbytes.xyz>
2026-05-13 11:05:52 +0100

    home manager release

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "iLikeToCode's NixOS config";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
