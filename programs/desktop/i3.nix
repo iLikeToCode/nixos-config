3f571c8cbad47256e39c27023c34deb83ca3503a
iLikeToCode <archie@archiesbytes.xyz>
2026-05-24 19:40:46 +0100

    redshift

--- programs/desktop/i3.nix
+++ programs/desktop/i3.nix
@@ -65,4 +65,17 @@
   services.displayManager.defaultSession = "none+i3";
 
   services.xserver.displayManager.lightdm.enable = true;
+  
+  location.provider = "geoclue2";
+  services.redshift = {
+    enable = true;
+    brightness = {
+      day = "1";
+      night = "0.5";
+    };
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+  };
 }
