3c32b42d2db9bd6ae53e3e7b83e7d6ff26a3278e
iLikeToCode <archie@archiesbytes.xyz>
2026-06-22 16:46:49 +0100

    fix assigns

--- programs/desktop/i3-config.nix
+++ programs/desktop/i3-config.nix
@@ -61,7 +61,7 @@ in
         "1" = [ { class = "firefox"; } ];
         "2" = [ { class = "Xfce4-terminal"; } ];
         "3" = [ { class = "Code"; } ];
-        "11" = [ { class = "PrismLauncher"; class = "^Minecraft"; } ];
+        "11" = [ { class = "PrismLauncher"; } { class = "^Minecraft"; } ];
         "17" = [ { class = "Spotify"; } ];
         "18" = [ { class = "Slack"; } ];
         "19" = [ { class = "element"; } ];
