1420177fcfda8ad6688f9f5e7a4faf393ced04f4
iLikeToCode <archie@archiesbytes.xyz>
2026-07-10 22:48:01 +0100

    allow new firefox extensions

--- programs/apps/firefox.nix
+++ programs/apps/firefox.nix
@@ -31,11 +31,9 @@ in
       DisplayMenuBar = "default-off";
       SearchBar = "unified";
       ExtensionSettings = {
-        "*".installation_mode = "blocked";
-
         # Bitwarden
         "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          installation_mode = "force_installed";
+          installation_mode = "normal_installed";
           install_url =
             "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
         };
